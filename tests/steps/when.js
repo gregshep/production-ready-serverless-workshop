db6fc7e8ef05a9287bc4463f927c3338d0f6c1e1
Greg <serverless@shepworld.com>
2023-07-21 15:59:37 -0600

    end of week 3 - EventBus and SQS adds, indv IAM perms

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,3 +1,4 @@
+const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge')
 const APP_ROOT = '../../'
 const _ = require('lodash')
 const aws4 = require('aws4')
@@ -5,6 +6,19 @@ const URL = require('url')
 const http = require('axios')
 
 const mode = process.env.TEST_MODE
+
+const viaEventBridge = async (busName, source, detailType, detail) => {
+    const eventBridge = new EventBridgeClient()
+    const putEventsCmd = new PutEventsCommand({
+        Entries: [{
+            Source: source,
+            DetailType: detailType,
+            Detail: JSON.stringify(detail),
+            EventBusName: busName
+        }]
+    })
+    await eventBridge.send(putEventsCmd)
+}
 
 const viaHandler = async (event, functionName) => {
     const handler = require(`${APP_ROOT}/functions/${functionName}`).handler
@@ -123,9 +137,11 @@ const we_invoke_notify_restaurant = async (event) => {
     if (mode === 'handler') {
         await viaHandler(event, 'notify-restaurant')
     } else {
-        throw new Error('not supported')
+        const busName = process.env.bus_name
+        await viaEventBridge(busName, event.source, event['detail-type'], event.detail)
     }
 }
+
 
 module.exports = {
     we_invoke_get_index,
